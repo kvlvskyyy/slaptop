6d7cd1add3b6ca3341538c7d381ee9645e549bc6
Oirschot,Veerle V.E. van <v.vanoirschot@student.fontys.nl>
2026-01-06 14:22:44 +0100

    fixing error

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 from functools import wraps
 from flask import session, flash, redirect, url_for
-from models import User
+from models import User, Order
 
 UPLOAD_FOLDER = "static/images/stickers"   # folder for stickers
 ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "webp"}
