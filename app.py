3cbc410e8511ce4cdfbfe2e6395ab113b370e721
woacwoak <woacwoak@gmail.com>
2026-01-14 11:49:10 +0100

    Production test3

--- app.py
+++ app.py
@@ -107,4 +107,6 @@ def create_app():
 
 app = create_app()
 
-app.run(debug=False)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
