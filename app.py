4c36ff6bfd3107872e43dac629a360a057b1d04f
woacwoak <woacwoak@gmail.com>
2026-01-19 14:50:09 +0100

    Added cloudinary configuration

--- app.py
+++ app.py
@@ -158,10 +158,5 @@ app = create_app()
 if __name__ == "__main__":
     app.run()
 
-cloudinary.config(
-    cloud_name = os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    api_key = os.environ.get('CLOUDINARY_API_KEY'),
-    api_secret = os.environ.get('CLOUDINARY_API_SECRET')
-)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
